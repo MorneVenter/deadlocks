2d359cc2be38332f62203da87915f2a9510681d8
Morne <morneventer.mv@gmail.com>
2019-05-31 20:52:57 +0200

    init new project

---
+++ deadlockApp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(deadlockApp.Startup))]
+namespace deadlockApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
