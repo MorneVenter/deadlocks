914709574ede6f33b6b2eb26332b566a746c984e
Morne <morneventer.mv@gmail.com>
2019-06-01 17:58:54 +0200

    Added writing to a file

--- deadlockApp/Default.aspx.cs
+++ deadlockApp/Default.aspx.cs
@@ -10,25 +10,36 @@ public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        using (StreamReader sr = new StreamReader(Server.MapPath("theFile.txt")))
-        {
-            string strContents = sr.ReadToEnd();
-            displayBox.Text = strContents;
-        }
-       
+        updatetext();
     }
 
 
     protected void btnGo_Click(object sender, EventArgs e)
     {
         displayBox.Text = firstWord.Text + " " + secondWord.Text;
+        string newStr = "";
 
         using (StreamReader sr = new StreamReader(Server.MapPath("theFile.txt")))
         {
             string strContents = sr.ReadToEnd();
-            strContents.Replace(firstWord.Text, secondWord.Text);
-            displayBox.Text = strContents;
+            newStr = strContents.Replace(firstWord.Text, secondWord.Text);
+           
         }
 
+        using (StreamWriter wr = new StreamWriter(Server.MapPath("theFile.txt"), false))
+        {
+            wr.WriteLine(newStr);
+        }
+
+        updatetext();
+    }
+
+    private void updatetext()
+    {
+        using (StreamReader sr = new StreamReader(Server.MapPath("theFile.txt")))
+        {
+            string strContents = sr.ReadToEnd();
+            displayBox.Text = strContents;
+        }
     }
 }
