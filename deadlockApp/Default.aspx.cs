23c3723b54537a96bedfee1a38ca08dccd48cefa
Morne <morneventer.mv@gmail.com>
2019-06-01 17:52:08 +0200

    added text replacment

--- deadlockApp/Default.aspx.cs
+++ deadlockApp/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,12 +10,25 @@ public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        displayBox.Text = Server.MapPath(".");
+        using (StreamReader sr = new StreamReader(Server.MapPath("theFile.txt")))
+        {
+            string strContents = sr.ReadToEnd();
+            displayBox.Text = strContents;
+        }
+       
     }
 
 
     protected void btnGo_Click(object sender, EventArgs e)
     {
         displayBox.Text = firstWord.Text + " " + secondWord.Text;
+
+        using (StreamReader sr = new StreamReader(Server.MapPath("theFile.txt")))
+        {
+            string strContents = sr.ReadToEnd();
+            strContents.Replace(firstWord.Text, secondWord.Text);
+            displayBox.Text = strContents;
+        }
+
     }
 }
